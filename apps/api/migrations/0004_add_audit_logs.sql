e445aaa234fe1342df9637e53ed3c40149d4bff0
nomorechokedboy <haiduongit@protonmail.com>
2026-08-31 20:35:20 +0700

    feat(api): impl audit log schema
    Impl audit log schema and generate migrations

---
+++ apps/api/migrations/0004_add_audit_logs.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `audit_logs` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`createdAt` text DEFAULT CURRENT_TIMESTAMP,
+	`updatedAt` text DEFAULT CURRENT_TIMESTAMP,
+	`actorUserId` integer,
+	`resource` text NOT NULL,
+	`action` text NOT NULL,
+	`resourceIds` text DEFAULT '[]',
+	`method` text NOT NULL,
+	`path` text NOT NULL,
+	`statusCode` integer,
+	`previousValue` text DEFAULT '{}',
+	`newValue` text DEFAULT '{}',
+	FOREIGN KEY (`actorUserId`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
---
+++ apps/api/schema/audit-logs.ts
@@ -0,0 +1,75 @@
+import { InferInsertModel, InferSelectModel, relations, sql } from 'drizzle-orm'
+import * as sqlite from 'drizzle-orm/sqlite-core'
+import { AppError } from '../errors'
+import { baseSchema } from './base'
+import { users } from './users'
+
+export type AuditAction = 'create' | 'update' | 'delete'
+
+const auditActions: AuditAction[] = ['create', 'update', 'delete']
+
+const AuditActionEnum = sqlite.customType<{
+	data: string
+	driverData: string
+}>({
+	dataType() {
+		return 'text'
+	},
+	toDriver(val: string) {
+		if (!auditActions.includes(val as AuditAction)) {
+			throw AppError.invalidArgument(
+				`action must be one of ${auditActions.join(', ')}`
+			)
+		}
+		return val
+	}
+})
+
+export const auditLogs = sqlite.sqliteTable('audit_logs', {
+	...baseSchema,
+	actorUserId: sqlite.int().references(() => users.id),
+	resource: sqlite.text().notNull(),
+	action: AuditActionEnum('action').$type<AuditAction>().notNull(),
+	resourceIds: sqlite
+		.text({ mode: 'json' })
+		.default(sql`'[]'`)
+		.$type<Array<number | string>>(),
+	method: sqlite.text().notNull(),
+	path: sqlite.text().notNull(),
+	statusCode: sqlite.int(),
+	previousValue: sqlite
+		.text({ mode: 'json' })
+		.default(sql`'{}'`)
+		.$type<unknown>(),
+	newValue: sqlite
+		.text({ mode: 'json' })
+		.default(sql`'{}'`)
+		.$type<unknown>()
+})
+
+export const auditLogsRelations = relations(auditLogs, ({ one }) => ({
+	actor: one(users, {
+		fields: [auditLogs.actorUserId],
+		references: [users.id]
+	})
+}))
+
+export type AuditLogDB = InferSelectModel<typeof auditLogs>
+
+export type AuditLogParams = InferInsertModel<typeof auditLogs>
+
+type auditLogRow = Omit<AuditLogDB, 'actorUserId'>
+
+export type AuditLog = auditLogRow & {
+	actor?: { id: number; displayName?: string } | null
+}
+
+export type AuditLogQuery = {
+	resource?: string
+	action?: AuditAction
+	actorUserId?: number
+	from?: string
+	to?: string
+	page?: number
+	pageSize?: number
+}
