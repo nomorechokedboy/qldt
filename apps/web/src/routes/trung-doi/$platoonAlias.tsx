dc2991b6b9c37ab9afae70bb6e12148582c50d2c
nomorechokedboy <haiduongit@protonmail.com>
2026-09-07 15:34:49 +0700

    feat(web): platoon now show its children troopers

--- apps/web/src/routes/trung-doi/$platoonAlias.tsx
+++ apps/web/src/routes/trung-doi/$platoonAlias.tsx
@@ -3,15 +3,8 @@ import ProtectedRoute from '@/components/ProtectedRoute'
 import CompanyFacilitiesTab from '@/components/company-facilities-tab'
 import CompanyWeaponsTab from '@/components/company-weapons-tab'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import UnitTroopersTable from '@/components/student-table/unit-trooper-table'
 import z from 'zod'
-import useUnitTroopersData from '@/hooks/useUnitTroopersData'
-import useOnDeleteStudents from '@/hooks/useOnDeleteStudents'
-import useActionColumn from '@/hooks/useActionColumn'
-import TableSkeleton from '@/components/table-skeleton'
-import { defaultBirthdayColumnVisibility } from '@/components/student-table/default-columns-visibility'
-import { battalionStudentColumnsWithoutAction } from '@/components/student-table/columns'
-import useUnitData from '@/hooks/useUnitData'
+import CompanyStudentTable from '@/components/company-student-table'
 
 const plattonAliasSearchSchema = z.object({ id: z.number().nonoptional() })
 
@@ -23,25 +16,6 @@ export const Route = createFileRoute('/trung-doi/$platoonAlias')({
 function RouteComponent() {
 	const { platoonAlias } = Route.useParams()
 	const { id } = Route.useSearch()
-	const {
-		data: troopers = [],
-		isLoading: isLoadingStudents,
-		refetch: refetchTroopers
-	} = useUnitTroopersData({ id })
-	const { data: unit } = useUnitData({
-		alias: platoonAlias,
-		level: 'platoon',
-		id
-	})
-	const filename = `danh-sach-quan-nhan-${platoonAlias}`
-
-	// const facetedFilters = useUnitFacetedFilters({ troopers, unit })
-	const handleDeleteTroopers = useOnDeleteStudents(refetchTroopers)
-	const actionColumn = useActionColumn(() => refetchTroopers())
-
-	if (isLoadingStudents) {
-		return <TableSkeleton />
-	}
 
 	return (
 		<ProtectedRoute>
@@ -58,30 +32,10 @@ function RouteComponent() {
 					</TabsList>
 
 					<TabsContent value='students'>
-						<UnitTroopersTable
-							params={{ id }}
-							columnVisibility={{
-								...defaultBirthdayColumnVisibility,
-								address: false,
-								status: false
-							}}
-							columns={[
-								...battalionStudentColumnsWithoutAction,
-								actionColumn
-							]}
-							// facetedFilters={facetedFilters}
-							placeholder='Chưa có thông tin quân nhân.'
-							exportConfig={{
-								filename,
-								defaultExportValues: {
-									unitName: unit?.parent?.name?.toUpperCase(),
-									underUnitName: unit?.name?.toUpperCase()
-								}
-							}}
-							onDeleteRows={handleDeleteTroopers}
-							onCreateSuccess={refetchTroopers}
-							enableCreation
-							showRefreshButton
+						<CompanyStudentTable
+							alias={platoonAlias}
+							level='platoon'
+							id={id}
 						/>
 					</TabsContent>
 
