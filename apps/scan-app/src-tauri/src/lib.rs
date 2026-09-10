c9131213f0ed99608ac29653f123eb9bbd92eb42
nomorechokedboy <haiduongit@protonmail.com>
2026-09-10 10:37:12 +0700

    fix(scanapp): fix the download button
    Instead of using fs, use the dialog plugin to save the QR image

--- apps/scan-app/src-tauri/src/lib.rs
+++ apps/scan-app/src-tauri/src/lib.rs
@@ -8,6 +8,7 @@ pub fn run() {
         .plugin(tauri_plugin_barcode_scanner::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_dialog::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
