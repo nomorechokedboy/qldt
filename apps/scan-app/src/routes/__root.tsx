a674e978979b618bb8b91a9abc1f535d66f4bd66
nomorechokedboy <haiduongit@protonmail.com>
2026-09-10 14:03:04 +0700

    chore(scanapp): adjust UI styling

--- apps/scan-app/src/routes/__root.tsx
+++ apps/scan-app/src/routes/__root.tsx
@@ -5,9 +5,12 @@ function RootLayout() {
 	return (
 		<>
 			<main className='bg-background mx-auto flex min-h-screen max-w-md flex-col gap-4 p-4'>
-				<h1 className='mt-2 text-center text-xl font-bold'>
-					Kiểm kê vũ khí/trang bị
-				</h1>
+				<div className='mt-2 flex flex-col items-center gap-1.5'>
+					<h1 className='text-center text-2xl font-semibold'>
+						Kiểm kê vũ khí/trang bị
+					</h1>
+					<div className='bg-primary h-[3px] w-10' />
+				</div>
 				<Outlet />
 			</main>
 			{import.meta.env.PROD !== true && <TanStackRouterDevtools />}
